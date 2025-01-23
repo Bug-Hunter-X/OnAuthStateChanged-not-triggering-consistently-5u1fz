5167866608d64e87fee68231b63bcecc07d0205b
BugHunter-X <askzenai@gmail.com>
2025-01-23 05:36:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    console.log('User is signed in:', uid);
+  } else {
+    // User is signed out
+    // ...
+    console.log('User is signed out');
+  }  
+});
+
+// unsubscribe from the listener when no longer needed
+// unsubscribe();
